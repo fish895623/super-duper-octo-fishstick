94d2deda8f3b55751a51bd2572d87c0b02b1ac2c
asdf <asdf@example.com>
2025-02-28 00:00:11 +0900

    AB

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,9 +2,21 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class DemoApplicationTests {
+	private static final String USERNAME = "username";
+	private static final String PASSWORD = "password";
+	private static final String DATABASE_NAME = "testdb";
+
+	static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0")
+			.withUsername(USERNAME)
+			.withPassword(PASSWORD)
+			.withDatabaseName(DATABASE_NAME);
 
 	@Test
 	void contextLoads() {
